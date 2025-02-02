91f10887310ad98b50720d53af663b529f1542d4
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2025-02-02 20:23:25 +0300

    keyboardBuilder

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,18 +1,35 @@
-from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButtonPollType
 from aiogram.filters import Command, CommandStart
 from lexicon.lexicon import LEXICON_RU
 from aiogram import Router, F
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 router = Router()
+
+#Создаем Бтлдел клавиатуры кнопок
+kb_builder = ReplyKeyboardBuilder()
 
 # Создаем кнопки
 button_1 = KeyboardButton(text='Собак 🦮')
 button_2 = KeyboardButton(text='Огурцов 🥒')
+contact_btn = KeyboardButton(text='Отправить номер телфона',request_contact=True)
+geo_btn = KeyboardButton(text='Отправить геолокацию', request_location=True)
+poll_btn = KeyboardButton(text='Создание опроса', request_poll=KeyboardButtonPollType())
 
-# Создаем клавиатуру для кнопок
+#Добавляем кнопки в билдер 
+kb_builder.row(button_1,button_2,contact_btn,geo_btn,poll_btn , width=5)
 
-keyboard = ReplyKeyboardMarkup(keyboard=[[button_1, button_2]], resize_keyboard=True,one_time_keyboard=True)
 
+# Создаем клавиатуру для кнопок Старое УЖЕ НЕАКТУАЛЬНО - ОСТАВЛЮ ДЛЯ ИСТОРИИ
+
+# keyboard = ReplyKeyboardMarkup(keyboard=[[button_1, button_2]], resize_keyboard=True,one_time_keyboard=True)
+
+# Создаем объект клавиатуры
+
+keyboard: ReplyKeyboardMarkup = kb_builder.as_markup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
 
 # Хэндлер обрботки команды Старт
 @router.message(CommandStart())
